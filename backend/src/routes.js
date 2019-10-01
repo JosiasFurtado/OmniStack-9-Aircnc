e230b980de04fd2705468bf13eccbc417cb46541
Josias Furtado <53015921+JosiasFurtado@users.noreply.github.com>
2019-10-01 13:44:11 -0300

    Add files via upload

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,17 @@
 const express = require('express')
-
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+const SessionController = require('./controllers/SessionC')
+const SpotController = require('./controllers/SpotC')
+const DashboardController = require('./controllers/DashboardC')
+const BookingController = require('./controllers/BookingC')
 const routes = express.Router()
+const upload = multer(uploadConfig)
 
-routes.get('/', (req, res) => {
-   return res.json({ message: "Hello World!!!"})
-})
+routes.get('/spots', SpotController.index)
+routes.get('/dashboard', DashboardController.show)
+routes.post('/sessions', SessionController.store)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 const routes = require('./routes')
 
-const app = express()
+
+mongoose.connect('mongodb+srv://omnistack9:omnistack9@cluster0-msjcp.mongodb.net/omnistack9?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 app.use(express.json())
 app.use(routes)
 
 app.listen(3333)
 
+//req.query = Acessar query params (filter)
+//req.params = Acessar route params (put && delete)
+//req.body = Acessar corpo da requisição(criação e edição)
